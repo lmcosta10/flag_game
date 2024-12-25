cf4f67b2828750574c5e2886fc685021849c7a83
lmcosta10 <letmc14@gmail.com>
2024-12-25 00:27:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Initial base code: from geeksforgeeks' pygame tutorials
+
+Countries and codes from https://gist.github.com/carlopires/1261951/d13ca7320a6abcd4b0aa800d351a31b54cefdff4
---
+++ flag_game.py
@@ -0,0 +1,327 @@
+import pygame
+import sys
+
+##############
+# TODO: move to a diff file
+
+# Countries and codes:
+countries_codes = {
+	'AD': 'Andorra',
+	'AE': 'United Arab Emirates',
+	'AF': 'Afghanistan',
+	'AG': 'Antigua & Barbuda',
+	'AI': 'Anguilla',
+	'AL': 'Albania',
+	'AM': 'Armenia',
+	'AN': 'Netherlands Antilles',
+	'AO': 'Angola',
+	'AQ': 'Antarctica',
+	'AR': 'Argentina',
+	'AS': 'American Samoa',
+	'AT': 'Austria',
+	'AU': 'Australia',
+	'AW': 'Aruba',
+	'AZ': 'Azerbaijan',
+	'BA': 'Bosnia and Herzegovina',
+	'BB': 'Barbados',
+	'BD': 'Bangladesh',
+	'BE': 'Belgium',
+	'BF': 'Burkina Faso',
+	'BG': 'Bulgaria',
+	'BH': 'Bahrain',
+	'BI': 'Burundi',
+	'BJ': 'Benin',
+	'BM': 'Bermuda',
+	'BN': 'Brunei Darussalam',
+	'BO': 'Bolivia',
+	'BR': 'Brazil',
+	'BS': 'Bahama',
+	'BT': 'Bhutan',
+	'BU': 'Burma (no longer exists)',
+	'BV': 'Bouvet Island',
+	'BW': 'Botswana',
+	'BY': 'Belarus',
+	'BZ': 'Belize',
+	'CA': 'Canada',
+	'CC': 'Cocos (Keeling) Islands',
+	'CF': 'Central African Republic',
+	'CG': 'Congo',
+	'CH': 'Switzerland',
+	'CI': 'Côte D\'ivoire (Ivory Coast)',
+	'CK': 'Cook Iislands',
+	'CL': 'Chile',
+	'CM': 'Cameroon',
+	'CN': 'China',
+	'CO': 'Colombia',
+	'CR': 'Costa Rica',
+	'CS': 'Czechoslovakia (no longer exists)',
+	'CU': 'Cuba',
+	'CV': 'Cape Verde',
+	'CX': 'Christmas Island',
+	'CY': 'Cyprus',
+	'CZ': 'Czech Republic',
+	'DD': 'German Democratic Republic (no longer exists)',
+	'DE': 'Germany',
+	'DJ': 'Djibouti',
+	'DK': 'Denmark',
+	'DM': 'Dominica',
+	'DO': 'Dominican Republic',
+	'DZ': 'Algeria',
+	'EC': 'Ecuador',
+	'EE': 'Estonia',
+	'EG': 'Egypt',
+	'EH': 'Western Sahara',
+	'ER': 'Eritrea',
+	'ES': 'Spain',
+	'ET': 'Ethiopia',
+	'FI': 'Finland',
+	'FJ': 'Fiji',
+	'FK': 'Falkland Islands (Malvinas)',
+	'FM': 'Micronesia',
+	'FO': 'Faroe Islands',
+	'FR': 'France',
+	'FX': 'France, Metropolitan',
+	'GA': 'Gabon',
+	'GB': 'United Kingdom (Great Britain)',
+	'GD': 'Grenada',
+	'GE': 'Georgia',
+	'GF': 'French Guiana',
+	'GH': 'Ghana',
+	'GI': 'Gibraltar',
+	'GL': 'Greenland',
+	'GM': 'Gambia',
+	'GN': 'Guinea',
+	'GP': 'Guadeloupe',
+	'GQ': 'Equatorial Guinea',
+	'GR': 'Greece',
+	'GS': 'South Georgia and the South Sandwich Islands',
+	'GT': 'Guatemala',
+	'GU': 'Guam',
+	'GW': 'Guinea-Bissau',
+	'GY': 'Guyana',
+	'HK': 'Hong Kong',
+	'HM': 'Heard & McDonald Islands',
+	'HN': 'Honduras',
+	'HR': 'Croatia',
+	'HT': 'Haiti',
+	'HU': 'Hungary',
+	'ID': 'Indonesia',
+	'IE': 'Ireland',
+	'IL': 'Israel',
+	'IN': 'India',
+	'IO': 'British Indian Ocean Territory',
+	'IQ': 'Iraq',
+	'IR': 'Islamic Republic of Iran',
+	'IS': 'Iceland',
+	'IT': 'Italy',
+	'JM': 'Jamaica',
+	'JO': 'Jordan',
+	'JP': 'Japan',
+	'KE': 'Kenya',
+	'KG': 'Kyrgyzstan',
+	'KH': 'Cambodia',
+	'KI': 'Kiribati',
+	'KM': 'Comoros',
+	'KN': 'St. Kitts and Nevis',
+	'KP': 'Korea, Democratic People\'s Republic of',
+	'KR': 'Korea, Republic of',
+	'KW': 'Kuwait',
+	'KY': 'Cayman Islands',
+	'KZ': 'Kazakhstan',
+	'LA': 'Lao People\'s Democratic Republic',
+	'LB': 'Lebanon',
+	'LC': 'Saint Lucia',
+	'LI': 'Liechtenstein',
+	'LK': 'Sri Lanka',
+	'LR': 'Liberia',
+	'LS': 'Lesotho',
+	'LT': 'Lithuania',
+	'LU': 'Luxembourg',
+	'LV': 'Latvia',
+	'LY': 'Libyan Arab Jamahiriya',
+	'MA': 'Morocco',
+	'MC': 'Monaco',
+	'MD': 'Moldova, Republic of',
+	'MG': 'Madagascar',
+	'MH': 'Marshall Islands',
+	'ML': 'Mali',
+	'MN': 'Mongolia',
+	'MM': 'Myanmar',
+	'MO': 'Macau',
+	'MP': 'Northern Mariana Islands',
+	'MQ': 'Martinique',
+	'MR': 'Mauritania',
+	'MS': 'Monserrat',
+	'MT': 'Malta',
+	'MU': 'Mauritius',
+	'MV': 'Maldives',
+	'MW': 'Malawi',
+	'MX': 'Mexico',
+	'MY': 'Malaysia',
+	'MZ': 'Mozambique',
+	'NA': 'Namibia',
+	'NC': 'New Caledonia',
+	'NE': 'Niger',
+	'NF': 'Norfolk Island',
+	'NG': 'Nigeria',
+	'NI': 'Nicaragua',
+	'NL': 'Netherlands',
+	'NO': 'Norway',
+	'NP': 'Nepal',
+	'NR': 'Nauru',
+	'NT': 'Neutral Zone (no longer exists)',
+	'NU': 'Niue',
+	'NZ': 'New Zealand',
+	'OM': 'Oman',
+	'PA': 'Panama',
+	'PE': 'Peru',
+	'PF': 'French Polynesia',
+	'PG': 'Papua New Guinea',
+	'PH': 'Philippines',
+	'PK': 'Pakistan',
+	'PL': 'Poland',
+	'PM': 'St. Pierre & Miquelon',
+	'PN': 'Pitcairn',
+	'PR': 'Puerto Rico',
+	'PT': 'Portugal',
+	'PW': 'Palau',
+	'PY': 'Paraguay',
+	'QA': 'Qatar',
+	'RE': 'Réunion',
+	'RO': 'Romania',
+	'RU': 'Russian Federation',
+	'RW': 'Rwanda',
+	'SA': 'Saudi Arabia',
+	'SB': 'Solomon Islands',
+	'SC': 'Seychelles',
+	'SD': 'Sudan',
+	'SE': 'Sweden',
+	'SG': 'Singapore',
+	'SH': 'St. Helena',
+	'SI': 'Slovenia',
+	'SJ': 'Svalbard & Jan Mayen Islands',
+	'SK': 'Slovakia',
+	'SL': 'Sierra Leone',
+	'SM': 'San Marino',
+	'SN': 'Senegal',
+	'SO': 'Somalia',
+	'SR': 'Suriname',
+	'ST': 'Sao Tome & Principe',
+	'SU': 'Union of Soviet Socialist Republics (no longer exists)',
+	'SV': 'El Salvador',
+	'SY': 'Syrian Arab Republic',
+	'SZ': 'Swaziland',
+	'TC': 'Turks & Caicos Islands',
+	'TD': 'Chad',
+	'TF': 'French Southern Territories',
+	'TG': 'Togo',
+	'TH': 'Thailand',
+	'TJ': 'Tajikistan',
+	'TK': 'Tokelau',
+	'TM': 'Turkmenistan',
+	'TN': 'Tunisia',
+	'TO': 'Tonga',
+	'TP': 'East Timor',
+	'TR': 'Turkey',
+	'TT': 'Trinidad & Tobago',
+	'TV': 'Tuvalu',
+	'TW': 'Taiwan, Province of China',
+	'TZ': 'Tanzania, United Republic of',
+	'UA': 'Ukraine',
+	'UG': 'Uganda',
+	'UM': 'United States Minor Outlying Islands',
+	'US': 'United States of America',
+	'UY': 'Uruguay',
+	'UZ': 'Uzbekistan',
+	'VA': 'Vatican City State (Holy See)',
+	'VC': 'St. Vincent & the Grenadines',
+	'VE': 'Venezuela',
+	'VG': 'British Virgin Islands',
+	'VI': 'United States Virgin Islands',
+	'VN': 'Viet Nam',
+	'VU': 'Vanuatu',
+	'WF': 'Wallis & Futuna Islands',
+	'WS': 'Samoa',
+	'YD': 'Democratic Yemen (no longer exists)',
+	'YE': 'Yemen',
+	'YT': 'Mayotte',
+	'YU': 'Yugoslavia',
+	'ZA': 'South Africa',
+	'ZM': 'Zambia',
+	'ZR': 'Zaire',
+	'ZW': 'Zimbabwe',
+	'ZZ': 'Unknown or unspecified country'
+}
+
+SCREEN_X = 600
+SCREEN_Y = 500
+
+##############
+
+pygame.init()
+
+screen = pygame.display.set_mode([SCREEN_X, SCREEN_Y])
+
+pygame.display.set_caption('image')
+
+imp = pygame.image.load(".\\country-flags\\ad.png").convert()
+flag_x = imp.get_width()
+flag_y = imp.get_height()
+flag_dim = (flag_x/2, flag_y/2)
+imp = pygame.transform.scale(imp, flag_dim)
+
+# Using blit to copy content from one surface to other
+screen.blit(imp, (0, 0))
+
+# paint screen one time
+pygame.display.flip()
+
+#####
+base_font = pygame.font.Font(None, 32) 
+user_text = ''
+input_rect = pygame.Rect(200, 200, 140, 32)
+color_active = pygame.Color('lightskyblue3')
+color_passive = pygame.Color('chartreuse4')
+color = color_passive
+
+active = False
+
+status = True
+while (status):
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            status = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            if input_rect.collidepoint(event.pos): 
+                active = True
+            else: 
+                active = False
+        
+        if event.type == pygame.KEYDOWN: 
+
+            if event.key == pygame.K_BACKSPACE: 
+                user_text = user_text[:-1]
+            else: 
+                user_text += event.unicode
+
+    if active: 
+        color = color_active 
+    else: 
+        color = color_passive 
+    
+    pygame.draw.rect(screen, color, input_rect) 
+  
+    text_surface = base_font.render(user_text, True, (255, 255, 255)) 
+    
+    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5)) 
+    
+    input_rect.w = max(100, text_surface.get_width()+10) 
+    
+    pygame.display.flip() 
+
+# deactivates the pygame library
+pygame.quit()
+sys.exit()
