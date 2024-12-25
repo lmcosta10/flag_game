49cb40e8bd2f7ce773c1e193047067b1fed76e40
lmcosta10 <letmc14@gmail.com>
2024-12-25 15:08:29 -0300

    indentation with tabs

--- flag_game.py
+++ flag_game.py
@@ -41,38 +41,37 @@ active = False
 status = True
 while (status):
 
-    for event in pygame.event.get():
+	for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
-            status = False
-        
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            if input_rect.collidepoint(event.pos): 
-                active = True
-            else: 
-                active = False
-        
-        if event.type == pygame.KEYDOWN: 
+		if event.type == pygame.QUIT:
+			status = False
 
-            if event.key == pygame.K_BACKSPACE: 
-                user_text = user_text[:-1]
-            else: 
-                user_text += event.unicode
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if input_rect.collidepoint(event.pos):
+				active = True
+			else:
+				active = False
 
-    if active: 
-        color = color_active 
-    else: 
-        color = color_passive 
-    
-    pygame.draw.rect(screen, color, input_rect) 
-  
-    text_surface = base_font.render(user_text, True, (255, 255, 255)) 
-    
-    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5)) 
-    
-    input_rect.w = max(100, text_surface.get_width()+10) 
-    
-    pygame.display.flip() 
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_BACKSPACE:
+				user_text = user_text[:-1]
+			else:
+				user_text += event.unicode
+
+	if active:
+		color = color_active
+	else:
+		color = color_passive
+
+	pygame.draw.rect(screen, color, input_rect)
+
+	text_surface = base_font.render(user_text, True, (255, 255, 255))
+
+	screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+
+	input_rect.w = max(100, text_surface.get_width()+10)
+
+	pygame.display.flip()
 
 # deactivates the pygame library
 pygame.quit()
